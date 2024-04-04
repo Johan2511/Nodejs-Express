13798e7bea1568476811ca6446a65a8b74028736
Johan2511 <johanpenuelah@gmail.com>
2024-04-04 13:36:12 -0500

    Creacion de codigos de estado htpp para la creacion, eliminacion y actualizacion de productos

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
+
 const productRouter = require ('./productsRouter');
 const usersRouter = require ('./userRouter');
 const categoriesRouter = require ('./categoriesRouter');
 
 function routerApi(app) {
-    app.use('/api/products', productRouter);
-    app.use('/api/users', usersRouter);
-    app.use('/api/categories', categoriesRouter);
+    const router = express.Router();
+    app.use('/api/v1', router)
+    router.use('/products', productRouter);
+    router.use('/users', usersRouter);
+    router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
